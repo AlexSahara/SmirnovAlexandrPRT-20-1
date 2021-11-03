2038401cfd717b965b9538fc3be45bb536dc29eb
AlexSahara <smirnovsasha2108@gmail.com>
2021-11-03 22:17:45 +0500

    Update Calc.cpp

--- Calc.cpp
+++ Calc.cpp
@@ -1,13 +1,20 @@
 ﻿#include <iostream>
+#include <conio.h>
 using namespace std;
 int main()
 {
 	int x = 0;
+	int A[5] = { 0, 0, 0, 0, 0 };
+	int B[5] = { 0, 0, 0, 0, 0 };
+	int C[5] = { 0, 0, 0, 0, 0 };
+	int D[5] = { 0, 0, 0, 0, 0 };
+	int E[5] = { 0, 0, 0, 0, 0 };
 
-	setlocale(LC_ALL, "rus");
 
 	for (;;)
 	{
+		setlocale(LC_ALL, "rus");
+		system("cls");
 
 		cout << "1. Ввод множества" << endl;
 		cout << "2. Вывод множества" << endl;
@@ -18,57 +25,98 @@ int main()
 		cout << "0. Выход" << endl;
 
 		cin >> x;
-		const int c = 5;
-
+		// const int c = 5;
 		switch (x)
 		{
 		case 1:
 			system("cls");
 			cout << " Введите множество A: " << endl;
-			int A[c];
-			for (int i = 0; i < c; i++)
+			for (int i = 0; i < 5; i++)
 			{
 				cin >> A[i];
 			}
-			return 0;
+			system("pause");
 			break;
 		case 2:
 			system("cls");
-			cout << " 2000 " << endl;
-			return 0;
+			cout << "A = [" ;
+			for (int i = 0; i < 4; i++)
+			{
+				cout << A[i] << ", ";
+			}
+			cout << A[4] << "]" << endl << endl;
+			system("pause");
 			break;
 		case 3:
 			system("cls");
-			cout << " 3000 " << endl;
-			return 0;
+			cout << " Введите число для проверки: ";
+			int j;
+			cin >> j;
+			for (int i = 0; i < 5; i++)
+			{
+				if (A[i] == j)
+					cout << endl << "Число " << j << " принадлежит множеству" << endl;
+			}
+			system("pause");
 			break;
 		case 4:
 			system("cls");
-			cout << " 4000 " << endl;
-			return 0;
+
+			cout << "A = [";
+			for (int i = 0; i < 4; i++)
+			{
+				cout << A[i] << ", ";
+			}
+			cout << A[4] << "]" << endl; 
+
+			cout << "B = [";
+			for (int i = 0; i < 4; i++)
+			{
+				cout << B[i] << ", ";
+			}
+			cout << B[4] << "]" << endl;
+
+			cout << "C = [";
+			for (int i = 0; i < 4; i++)
+			{
+				cout << C[i] << ", ";
+			}
+			cout << C[4] << "]" << endl;
+
+			cout << "D = [";
+			for (int i = 0; i < 4; i++)
+			{
+				cout << D[i] << ", ";
+			}
+			cout << D[4] << "]" << endl;
+
+			cout << "E = [";
+			for (int i = 0; i < 4; i++)
+			{
+				cout << E[i] << ", ";
+			}
+			cout << E[4] << "]" << endl;
+
+			system("pause");
 			break;
 		case 5:
 			system("cls");
 			cout << " 4000 " << endl;
-			return 0;
 			break;
 		case 6:
 			system("cls");
 			cout << " 4000 " << endl;
-			return 0;
 			break;
 		case 0:
 			system("cls");
-			cout << " 4000 " << endl;
 			return 0;
-			break;
 		default: 
 
 			system("cls");
+			cout << "Вы выбрали несуществующий пункт меню!" << endl;
+
 		}
 
-		system("cls");
-		cout << "Вы выбрали несуществующий пункт меню!" << endl;
 
 	}
 
